e7354b268c4faa1fb309ea02a1b7654386b9097a
jonambas <jon@jonambas.com>
2023-11-15 19:21:55 -0500

    fix: Layout and Config type exports

--- api/index.d.ts
+++ api/index.d.ts
@@ -1,9 +1,11 @@
 /* eslint-disable no-var */
 
-import type { Add, Directory } from './types';
+import type { Add, Directory, Config, LibraLayout } from './types';
 
 declare global {
   var add: Add;
   var directory: Directory;
   var describe: Directory;
 }
+
+export type { Config, LibraLayout };
